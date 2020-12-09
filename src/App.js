51ce81e0a5775edf9b4f92381a8e263d94349107
Rory <rohoulding@googlemail.com>
2020-12-09 17:05:53 -0500

    Started work on button component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// Components
+import Button from './components/button';
 
 function App() {
   return (
-    <h1>Hello!</h1>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Button>Join game</Button>
+          <div>Home</div>
+        </Route>
+        <Route path="/new" exact>
+          <div>New game</div>
+        </Route>
+        <Route path="/join" exact>
+          <div>Join a game</div>
+        </Route>
+        <Route>
+          <div>404</div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
